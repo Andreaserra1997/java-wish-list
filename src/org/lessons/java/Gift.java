50950917eae73206720adcd56f5435bdbee58ab4
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-10-26 14:58:13 +0200

    add list Gift

--- src/org/lessons/java/Gift.java
+++ src/org/lessons/java/Gift.java
@@ -1,4 +1,37 @@
 package org.lessons.java;
 
 public class Gift {
+    //ATTRIBUTI
+    private String description;
+    private String receiver;
+
+    //COSTRUTTORI
+    public Gift(String description, String receiver) {
+        this.description = description;
+        this.receiver = receiver;
+    }
+
+    //GETTER E SETTER
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+    @Override
+    public String toString() {
+        return "Gift{" +
+                "description='" + description + '\'' +
+                ", receiver='" + receiver + '\'' +
+                '}';
+    }
 }
--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,7 +1,31 @@
 package org.lessons.java;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        List<Gift> gift = new ArrayList<>();
+
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("Vuoi inserire un nuovo regalo? y/n");
+            exit = scan.nextLine().equals("n");
+            if (!exit) {
+                System.out.println("Descrizione del regalo: ");
+                String description = scan.nextLine();
+                System.out.println("Destinatario del regalo: ");
+                String receiver = scan.nextLine();
+                gift.add(new Gift(description, receiver));
+                System.out.println("La tua lista contiene " + gift.size() + " regali");
+            }
+        }
+        System.out.println(gift);
+
+        scan.close();
     }
 }
